7a0f49864370d81cd8eee63957ba0e5402644821
Anders Logg <logg@simula.no>
2010-12-16 14:40:50 +0000

    More work on spell-checking

--- spellcheck.sh
+++ spellcheck.sh
@@ -3,6 +3,6 @@
 for f in chapters/*; do
     echo "Spell checking $f"
     echo "------------------------------------"
-    cat $f/chapter.tex | utils/stripfile | ispell -l -t -d american -p`pwd`/dictionary.txt
+    cat $f/chapter.tex | utils/stripfile | grep -v includegraphics | ispell -l -t -d american -p`pwd`/dictionary.txt
     echo ""
 done
