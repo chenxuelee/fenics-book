051a834078fa5244bd9eee016cbce33abe58a4f2
Anders Logg <logg@simula.no>
2011-10-25 15:51:34 +0200

    Update spellchecking script to work with new directory structure

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-for f in `ls chapters`; do
-    echo "Spell checking chapters/$f/chapter.tex"
-    echo "--------------------------------------------"
+#for f in `ls chapters`; do
+#    echo "Spell checking chapters/$f/chapter.tex"
+#    echo "--------------------------------------------"
+#    ./spellcheckchapter.sh $f
+#    echo ""
+#done
+
+for f in `ls tex/*.tex`; do
+    echo "Spell checking $f"
+    echo "---------------------"
     ./spellcheckchapter.sh $f
     echo ""
 done
--- spellcheckchapter.sh
+++ spellcheckchapter.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cat chapters/$1/chapter.tex | utils/stripfile | ispell -l -t -d american -p`pwd`/dictionary.txt
+#cat chapters/$1/chapter.tex | utils/stripfile | ispell -l -t -d american -p`pwd`/dictionary.txt
+
+cat $1 | utils/stripfile | ispell -l -t -d american -p`pwd`/dictionary.txt
